879759a43e73a3dff7391595c19d0deb9f3f0718
omennia <ianginthnemo@gmail.com>
2022-04-17 14:19:45 +0100

    fixing colors

--- Util.h
+++ Util.h
@@ -19,8 +19,11 @@ namespace Util {
 int CountSegments(array<array<char, WIDTH>, HEIGHT> &board, pair<int, int> pos, pair<int, int> dir);
 bool MakeMove(int col, Game &game, char symbol);
 void PrintGame(Game &game);
+void PrintVictoriousGame(Game &game);
 void CreateChildren(Game &game, vector<Game> &children, char symbol);
 bool CheckForWin(Game &game, int col);
+bool IsVictoriousPiece(vector<pair<int, int>> &won_positions, int row, int col);
+vector<pair<int, int>> GetWinSegment(Game &game, int col);
 int UtilityFunction(Game &game, int move);
 int GetValueForSegment(pair<int, int> segment_results);
 char GetNextSymbol(char symbol);
